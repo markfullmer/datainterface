337f5ecd29e13cf8873cf7252829caf57daa4680
Mark Fullmer <mfullmer@gmail.com>
2022-04-17 13:59:50 -0500

    Add sheet music flavor

--- index.php
+++ index.php
@@ -13,27 +13,32 @@ $records = $stmt->process($csv);
 
 $artists = [];
 foreach ($records as $record) {
-  $artists[] = trim($record['Artist']);
+  $artists[] = trim($record['Composer']);
 }
 $artists = array_unique($artists);
 
 echo '<form action="//' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] . '" method="POST">';
 echo '<select name="artist">';
+echo '<option value="">All</option>';
 foreach ($artists as $i) {
   echo '<option value="' .  $i. '">' . $i . '</option>';
 }
 echo '</select>';
-echo '<input type="submit" value="Filter by artist"><button><a href="//' . $_SERVER['SERVER_NAME'] . '">Reset</a></button></form>';
+echo '<input type="submit" value="Filter by composer"><button><a href="//' . $_SERVER['SERVER_NAME'] . '">Reset</a></button></form>';
 
 if (isset($_POST['artist'])) {
   $artist = $_POST['artist'];
 }
 
-echo '<table><thead><th>Artist</th><th>Title</th><th>Year</th><th>Label</th><th>Genre</th><th>Cover Art</th></thead>';
+echo '<table><thead><th>Composer</th><th>Title</th><th>Year</th></thead>';
 foreach ($records as $record) {
-  if (isset($artist) && $record['Artist'] !== $artist) {
+  if (isset($artist) && $artist !== "" && $record['Composer'] !== $artist) {
     continue;
   }
-  echo '<tr><td>' . $record['Artist'] . '</td><td>' . $record['Title'] . '</td><td>' . $record['Year'] . '</td><td>' . $record['Label'] . '</td><td>' . $record['Genre'] . '</td><td><img src="' . $record['Image'] . '" width="200" height="200" /></td></tr>';
+  $td = [];
+  $td[] = '<tr><td>' . $record['Composer'] . '</td>';
+  $td[] = '<td>' . $record['Title'] . '</td>';
+  $td[] = '<td>' . $record['Copyright'] . '</td></tr>';
+  echo implode($td);
 }
 echo '</table>';
