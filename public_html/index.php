01bc06b1f82bc3a03e130658f4fc58a538b66d28
Mark Fullmer <mfullmer@gmail.com>
2022-09-18 15:41:49 -0700

    Update instructions

--- public_html/index.php
+++ public_html/index.php
@@ -19,20 +19,34 @@ require 'head.php';
     </ul>
     <h3>Specifications & Usage</h3>
     1. Create a standard-format CSV file with a header row. A simple example is below:
-    <pre><code>Artist,Title,Year
+    <pre><code>Artist,Title,Year,Genre
 America,America,1971
 Eric Andersen,Bout Changes 'n' Things Take 2,1966</code></pre>
     2. Add this PHP library to your project in the standard way:
-<pre><code>composer require markfullmer/datainterface</code></pre>
+    <pre><code>composer require markfullmer/datainterface</code></pre>
     3. Instantiate the data as a PHP class object. In the example below, the code is being instructed to create a filter for 'Year' and 'Genre' (but not 'Artist') and to display 'Artist', 'Title', and 'Year' in the table:
     <pre><code>&lt;?php
 use markfullmer\datainterface\DataInterface;
 
-$app = new DataInterface('records.csv', [
+$app = new DataInterface([
+  'file' => 'records.csv',
   'title' => 'List of LPs',
   'filters' => ['Year', 'Genre'],
   'table_columns' => ['Artist', 'Title', 'Year'],
 ]);</code></pre>
+
+The library also supports reading CSV data over HTTP (including public, published Google Sheets as CSV). To use this approach, provide a "url" parameter:
+<pre><code>
+$app = new DataInterface([
+  'file' => 'https://example.com/records.csv',
+  'title' => 'List of LPs',
+  'filters' => ['Year', 'Genre'],
+  'table_columns' => ['Artist', 'Title', 'Year'],
+]);
+</code></pre>
+
+If reading data over HTTP, the library stores the URL in the filesystem for performance reasons. To refresh the request, add `?reset` to the website URL.
+
     4. Render the parts of the interface as desired:
     <pre><code>&lt;?php
 echo $app->options['title'];
